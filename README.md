4cd2f8a0964fecdf01999bdc515a9ad3eb606ca8
shidongzhao <shidongzhao@100tal.com>
2019-09-22 14:15:12 +0800

    上传

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# resume_wuyujiesong
- 
+### Hack-hu 的个人简历
+
+#### 个人简历
+[在线地址](https://hackhu2019.github.io/resume/)
+
+
---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: 石东昭
+ * @Date: 2019-09-22 14:05:04
+ * @LastEditors: 石东昭
+ * @LastEditTime: 2019-09-22 14:05:04
+ */
+window.onload = function(){
+	console.log("感谢您的浏览，期待能够加入贵公司！我的 GitHub：https://github.com/LeishenKOBE");
+}
